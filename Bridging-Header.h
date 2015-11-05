395412294acc1ce2869a529acdceb5def22dc4f8
wzpziyi1 <1204473090@qq.com>
2015-11-05 09:35:19 +0800

    first commit

---
+++ Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  无限循环播放图片
+//
+//  Created by 王志盼 on 15/11/4.
+//  Copyright © 2015年 王志盼. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
---
+++ 无限循环播放图片/ZYConst.swift
@@ -0,0 +1,13 @@
+//
+//  ZYConst.swift
+//  无限循环播放图片
+//
+//  Created by 王志盼 on 15/11/4.
+//  Copyright © 2015年 王志盼. All rights reserved.
+//
+
+import UIKit
+
+class ZYConst: NSObject {
+
+}
